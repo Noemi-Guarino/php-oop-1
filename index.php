b9248da3a01d0ac2c0bbcb5531782ff072c80237
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-02-16 16:58:51 +0100

    compleate

--- index.php
+++ index.php
@@ -12,12 +12,31 @@ class Movie {
         $this->genre = $genre;
         $this->year = $year;
     }
+
+    // Metodo per stampare le informazioni del film
+    public function printInfo() {
+        echo "Title: $this->title <br>";
+        echo "Genre: $this->genre <br>";
+        echo "Year: $this->year <br>";
+        echo "<br>";
+    }
+    
 }
 
 // Creazione di due oggetti Movie
 $movie1 = new Movie("Fight Club","Thriller", 1999);
 $movie2 = new Movie("Fuori di cresta SLC Punk!", "Commedia", 1998);
+
+// Stampare le informazioni dei film
+echo "<h2>Movie 1:</h2>";
+$movie1->printInfo();
+
+echo "<h2>Movie 2:</h2>";
+$movie2->printInfo();
+
 ?>
+
+
 
 
 <!DOCTYPE html>
