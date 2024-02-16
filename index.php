29e2ea0efe04874bf461bb0d2843255880c4f319
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-02-16 16:39:53 +0100

    edit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// Definizione della classe Movie
+class Movie {
+    // Variabili di istanza
+    public $title;
+    public $genre;
+    public $year;
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP 1</title>
+</head>
+<body>
+    
+</body>
+</html>
